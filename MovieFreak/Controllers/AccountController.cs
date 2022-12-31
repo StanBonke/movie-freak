0cfc0ca96848c86429e2c27f9d10e6a0194552e9
Stan <r0753034@student.thomasmore.be>
2022-12-31 16:27:45 +0100

    last fixes

--- MovieFreak/Controllers/AccountController.cs
+++ MovieFreak/Controllers/AccountController.cs
@@ -1,22 +1,70 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MovieFreak.Data;
+using MovieFreak.Models;
+using MovieFreak.ViewModels.AccountViewModels;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace MovieFreak.Controllers
 {
+    [Authorize(Roles = "user")]
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
+        private readonly MfContext _context;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(MfContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         // INDEX
         public IActionResult Index()
         {
-            return View();
+            List<Film> films = _context.Films
+                .Include(g => g.Genre)
+                .ToList();
+
+            Genre genre = _context.Genres
+                .FirstOrDefault();
+
+            FavoritesViewModel vm = new FavoritesViewModel()
+            {
+                Films = films,
+                Genre = genre
+            };
+
+            return View(vm);
+        }
+
+        // SEARCH
+        public IActionResult Search(FavoritesViewModel vm)
+        {
+            // MULTIPLE PROPERTY SEARCH
+
+            var query = _context.Films.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(vm.Search))
+            {
+                string[] search = vm.Search.Split(' ', ',');
+                foreach (var item in search)
+                {
+                    query = query.Where(x =>
+                        x.Titel.Contains(item) ||
+                        x.Genre.FilmGenre.Contains(item))
+                        .Include(g => g.Genre);
+                }
+
+                vm.Films = query.ToList();
+            }
+            else
+            {
+                vm.Films = query.Include(g => g.Genre).ToList();
+            }
+
+            return View("Index", vm);
         }
     }
 }
---
+++ MovieFreak/ViewModels/AccountViewModels/FavoritesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MovieFreak.Models;
+using System.Collections.Generic;
+
+namespace MovieFreak.ViewModels.AccountViewModels
+{
+    public class FavoritesViewModel
+    {
+        public List<Film> Films { get; set; }
+        public Genre Genre { get; set; }
+
+        // SEARCH
+        public string Search { get; set; }
+    }
+}
